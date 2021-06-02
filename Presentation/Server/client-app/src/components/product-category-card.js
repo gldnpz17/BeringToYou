7866d8465c1431f4f5000739086ee762f361fdf0
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-06-02 21:00:45 +0700

    Added maximum width to the product card.

--- Presentation/Server/client-app/src/components/product-category-card.js
+++ Presentation/Server/client-app/src/components/product-category-card.js
@@ -7,10 +7,15 @@ const StyledCard = styled(CustomCard)`
   color: black;
 `;
 
+const ContentContainer = styled(Card.Body)`
+  padding: 0;
+  display: grid;
+`;
+
 const ProductCategoryCard = ({ image, name, itemCount, style, className, onClick }) => {
   return (
     <StyledCard className={`m-0 ${className}`} style={style} onClick={onClick}>
-      <Card.Body className='p-0 d-flex flex-column h-100'>
+      <Card.Body className='p-0 d-flex flex-column h-100 w-100'>
         <img className='flex-grow-1' src='dummy-images/vegetables.jpg' style={{objectFit: 'cover'}} />
         <div className='p-1'>
           <p className='m-0'><b>{name}</b></p>
