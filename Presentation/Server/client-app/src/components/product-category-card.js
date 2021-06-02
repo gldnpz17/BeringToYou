dfe4d9be7c0120a8c9df15738ed8259dbf23860e
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-06-02 21:25:10 +0700

    Set product category card image width to 100%.

--- Presentation/Server/client-app/src/components/product-category-card.js
+++ Presentation/Server/client-app/src/components/product-category-card.js
@@ -8,20 +8,22 @@ const StyledCard = styled(CustomCard)`
 `;
 
 const ContentContainer = styled(Card.Body)`
-  padding: 0;
-  display: grid;
+  img {
+    width: 100%;
+    object-fit: cover;
+  }
 `;
 
 const ProductCategoryCard = ({ image, name, itemCount, style, className, onClick }) => {
   return (
     <StyledCard className={`m-0 ${className}`} style={style} onClick={onClick}>
-      <Card.Body className='p-0 d-flex flex-column h-100 w-100'>
-        <img className='flex-grow-1' src='dummy-images/vegetables.jpg' style={{objectFit: 'cover'}} />
+      <ContentContainer className='p-0 d-flex flex-column h-100'>
+        <img className='flex-grow-1' src='dummy-images/vegetables.jpg' />
         <div className='p-1'>
           <p className='m-0'><b>{name}</b></p>
           <p className='m-0'>123 Items</p>
         </div>
-      </Card.Body>
+      </ContentContainer>
     </StyledCard>
   );
 };
