431b49216692f55dff699f3f8daefa8fabd80932
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-07-31 18:59:30 +0700

    Add migrations.

---
+++ Infrastructure/PostgresDatabase/Migrations/20210731115332_AddLowerCaseShopNameAndDescription.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostgresDatabase.Migrations
+{
+    public partial class AddLowerCaseShopNameAndDescription : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LowercaseDescription",
+                table: "Shops",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LowercaseName",
+                table: "Shops",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LowercaseDescription",
+                table: "Shops");
+
+            migrationBuilder.DropColumn(
+                name: "LowercaseName",
+                table: "Shops");
+        }
+    }
+}
