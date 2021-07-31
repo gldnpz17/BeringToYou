84949d827de1515acf32008afa9133f17431c125
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-07-31 15:02:43 +0700

    Added DB migrations.

---
+++ Infrastructure/PostgresDatabase/Migrations/20210731072012_ReviseDomainModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostgresDatabase.Migrations
+{
+    public partial class ReviseDomainModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "MaxPrice",
+                table: "Shops",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "MinPrice",
+                table: "Shops",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxPrice",
+                table: "Shops");
+
+            migrationBuilder.DropColumn(
+                name: "MinPrice",
+                table: "Shops");
+        }
+    }
+}
