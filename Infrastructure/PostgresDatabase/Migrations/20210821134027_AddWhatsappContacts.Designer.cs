81952ad641fa900634bc34030313a2366884fe59
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-08-21 20:46:16 +0700

    Migrate database.

--- Infrastructure/PostgresDatabase/Migrations/20210821134027_AddWhatsappContacts.Designer.cs
+++ Infrastructure/PostgresDatabase/Migrations/20210821134027_AddWhatsappContacts.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using PostgresDatabase;
@@ -9,9 +10,10 @@ using PostgresDatabase;
 namespace PostgresDatabase.Migrations
 {
     [DbContext(typeof(PostgresAppDbContext))]
-    partial class PostgresAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210821134027_AddWhatsappContacts")]
+    partial class AddWhatsappContacts
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure/PostgresDatabase/Migrations/20210821134027_AddWhatsappContacts.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PostgresDatabase.Migrations
+{
+    public partial class AddWhatsappContacts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "LowercaseName",
+                table: "ShopSubcategories",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LowercaseName",
+                table: "ShopCategories",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "ShopContacts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ContactIdentity = table.Column<string>(type: "text", nullable: true),
+                    ContactUrl = table.Column<string>(type: "text", nullable: true),
+                    Discriminator = table.Column<string>(type: "text", nullable: false),
+                    ShopId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopContacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShopContacts_Shops_ShopId",
+                        column: x => x.ShopId,
+                        principalTable: "Shops",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopContacts_ShopId",
+                table: "ShopContacts",
+                column: "ShopId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShopContacts");
+
+            migrationBuilder.DropColumn(
+                name: "LowercaseName",
+                table: "ShopSubcategories");
+
+            migrationBuilder.DropColumn(
+                name: "LowercaseName",
+                table: "ShopCategories");
+        }
+    }
+}
